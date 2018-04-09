de2a1f1142f0330ba385fc517854328d3593a416
Julius von Froreich <fvj@users.noreply.github.com>
2018-04-09 20:18:22 +0200

    implement rds, format, remove sbimap

--- src/index.js
+++ src/index.js
@@ -1,71 +1,93 @@
 const route = require('spirit-router')
 const http = require('http')
-const {adapter, redirect} = require('spirit').node
-const {randomBytes} = require('crypto')
-const {default: body} = require('spirit-body')
-const {parse: parseString} = require('querystring')
-const {default: SimpleBiMap} = require('./sbimap')
-const {existsSync: fileExists} = require('fs')
+const { adapter, redirect } = require('spirit').node
+const { randomBytes } = require('crypto')
+const { defaults } = require('spirit-common')
+const { Pool, Client } = require('pg')
+require('dotenv').load()
+
+const middleware = defaults('api') // we don't need sessions for now
+const JSON_USER_AGENT_FRAGMENTS = ['curl', 'httpie'] // user agent fragments, matching user-agents will receive json
 
 const notFound = {
 	status: 404,
 	body: 'Not Found',
-	headers: { 'Content-Type': 'text/plain; charset=utf-8' }
+	headers: { 'Content-Type': 'text/plain; charset=utf-8' },
 }
+
+const badRequest = errors => ({
+	status: 400,
+	headers: { 'Content-Type': 'application/json; charset=utf-8' },
+	body: JSON.stringify(errors),
+})
 
 const index = `
 	<form method="post">
 		<input type="text" name="url" />
 		<input type="submit" />
-	</form>`.trim().replace(/\t/g, '')
+	</form>`
+	.trim()
+	.replace(/\t/g, '')
 
-const lookup = (url) => {
-	if (mapping.has(url))
-		return redirect(mapping.get(url))
-	return notFound
+const generateId = () =>
+	Math.random()
+		.toString(36)
+		.slice(2, 12) // max-length of 10 (12-2)
+
+const lookup = id => {
+	return pool.query('SELECT url FROM links WHERE id=$1;', [id]).then(res => {
+		if (res.rows.length > 0 && 'url' in res.rows[0])
+			return redirect(res.rows[0].url)
+		return notFound
+	})
 }
 
-const save = (body, {host}, protocol) => {
-	const {url} = parseString(body)
+const save = (body, headers, protocol, ip) => {
+	const { url } = body
+	const userAgent = headers['user-agent'].toLowerCase()
+	let id = generateId()
 
-	if (mapping.has(url))
-		return `<input type="text" onclick="this.select();"
-			 value="${protocol}://${host}/${mapping.get(url)} readyonly />`
+	if (!url) return badRequest({ url: 'missing field' })
 
-	let token = randomBytes(4).toString('hex')
-	while (mapping.has(token))
-		token = randomBytes(4).toString('hex')
+	return pool
+		.query(
+			'INSERT INTO links (id, url, ip, created_at, updated_at) VALUES ($1, $2, $3, current_timestamp, current_timestamp) RETURNING id;',
+			[id, url, ip]
+		)
+		.then(res => {
+			if (res.rows.length === 0 || !('id' in res.rows[0]))
+				return { status: 500, body: 'Internal server error' }
 
-	mapping.set(url, token)
+			const tiny = `${protocol}://${headers.host}/${res.rows[0].id}`
 
-	return `<input type="text" onclick="this.select();"
-		value="${protocol}://${host}/${mapping.get(url)}" readonly />`
+			if (
+				JSON_USER_AGENT_FRAGMENTS.find(
+					fragment => userAgent.indexOf(fragment) != -1
+				)
+			)
+				return {
+					status: 200,
+					headers: { 'Content-Type': 'application/json; charset=utf-8' },
+					body: tiny,
+				}
+
+			return `<input type="text" onclick="this.select();" value="${tiny}" readonly />`
+		})
 }
 
-const constructApp = (path) => {
-	if (!fileExists(path))
-		(new SimpleBiMap()).dumpToFile(path)
-	mapping = new SimpleBiMap().importFromFile(path)
-	return route.define([
-		route.get('/', index),
-		route.wrap(route.post('/', ['body', 'headers', 'protocol'], save), [body]),
-		route.get('/:url', ['url'], lookup),
-		route.any('*', notFound)
-	])
-}
-
-const databasePath = './database.json'
 const port = 3000
-let mapping = undefined
-const app = constructApp(databasePath)
+const app = route.define([
+	route.get('/', index),
+	route.wrap(route.post('/', ['body', 'headers', 'protocol', 'ip'], save), [
+		middleware,
+	]),
+	route.get('/:id', ['id'], lookup),
+	route.any('*', notFound),
+])
+
+// gets its configuration from environment variables (PGHOST, PGUSER, PGPASSWORD, PGDATABASE, PGPORT)
+const pool = new Pool()
 
 http.createServer(adapter(app)).listen(port, () => {
-	console.log(`Running server on port ${port}, database path: ${databasePath}`)
-})
-
-process.on('SIGINT', () => {
-	console.log(`Caught SIGINT. Dumping database to ${databasePath}`)
-	mapping.dumpToFile(databasePath)
-	console.log(`Dump complete. Godspeed.`)
-	process.exit(1)
+	console.log(`Running server on port ${port}`)
 })
